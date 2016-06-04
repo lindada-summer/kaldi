4807836488d9d956a0b199b90feb695848f2c551
Hossein Hadian <hn.hadian@gmail.com>
2016-06-05 00:56:16 +0430

    Add on-GPU computation codes for numerator

--- src/chain/chain-kernels-ansi.h
+++ src/chain/chain-kernels-ansi.h
@@ -48,6 +48,33 @@ extern "C" {
                               const BaseFloat *prev_alpha,
                               BaseFloat *this_alpha);
 
+
+
+  void cuda_chain_num_hmm_backward(dim3 Gr, dim3 Bl,
+                               const Int32Pair *forward_transitions,
+                               const DenominatorGraphTransition *transitions,
+                               int32_cuda num_sequences,
+                               const int32_cuda *num_states,
+                               int32_cuda max_num_hmm_states,
+                               const BaseFloat *probs,
+                               int32_cuda prob_stride,
+                               const BaseFloat *this_alpha,
+                               const BaseFloat *next_beta,
+                               BaseFloat *this_beta,
+                               BaseFloat *log_prob_deriv,
+                               int32_cuda log_prob_deriv_stride);
+
+  void cuda_chain_num_hmm_forward(dim3 Gr, dim3 Bl,
+                              const Int32Pair *backward_transitions,
+                              const DenominatorGraphTransition *transitions,
+                              int32_cuda num_sequences,
+                              const int32_cuda *num_states,
+                              int32_cuda max_num_hmm_states,
+                              const BaseFloat *probs,
+                              int32_cuda prob_stride,
+                              const BaseFloat *prev_alpha,
+                              BaseFloat *this_alpha);
+
 } // extern "C"
 
 #endif  // HAVE_CUDA
